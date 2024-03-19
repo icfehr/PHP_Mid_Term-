673b64377a170b02efa6f18486124e112cc0a223
icfehr <73271748+icfehr@users.noreply.github.com>
2024-03-19 03:35:41 -0400

    Copy files from base repo to solve issue

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
+declare(strict_types = 1);
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
-
 $method = $_SERVER['REQUEST_METHOD'];
 
 if ($method === 'OPTIONS') {
@@ -9,30 +9,11 @@ if ($method === 'OPTIONS') {
     header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
     exit();
 }
-
+echo '<p>Hello World</p>';
 echo '<pre>';
 print_r(getenv('SITE_URL'));
 echo '<br>';
 print_r($_SERVER);
 echo '</pre>';
-
+phpinfo();
 ?>
-  
-<html>
-  <head>
-    <title>PHP Test</title>
-  </head>
-  <body>
-    <?php echo '<p>Hello World</p>'; ?> 
-    <?php phpinfo(); ?> 
-
-    
-
-  <!--
-  This script places a badge on your repl's full-browser view back to your repl's cover
-  page. Try various colors for the theme: dark, light, red, orange, yellow, lime, green,
-  teal, blue, blurple, magenta, pink!
-  -->
-  <script src="https://replit.com/public/js/replit-badge-v2.js" theme="dark" position="bottom-right"></script>
-  </body>
-</html>
