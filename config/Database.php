6c454a727af5237acfd1ea95fb555f49060317b3
icfehr <73271748+icfehr@users.noreply.github.com>
2024-03-20 11:41:42 -0400

    pushing env change hopefully

--- config/Database.php
+++ config/Database.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Dotenv\Dotenv;
 
-$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv::createImmutable('../');
 $dotenv->load();
 
 class Database {
